b2c4ae2573ef275df23780da028328cdaf054155
Anditem0 <89792824+Anditem0@users.noreply.github.com>
2026-01-28 10:35:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tên repository của bạn là 'lixi', nên base phải để như sau:
-  base: '/lixi/', 
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+  base: '/lixi/', // Phải có dấu gạch chéo ở đầu và cuối
 })
